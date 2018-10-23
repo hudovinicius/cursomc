4cbc4bdd0a33687ba1805fa7d585be94fa51f428
Hudo <hudovinicius@gmail.com>
2018-10-23 11:18:55 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/hudosalvador/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.hudosalvador.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hudosalvador.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
